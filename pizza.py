0fc1b78464beee0f9a73ecb4e59636dc4eafa1aa
gndukanma <gndukanma@villa.edum>
2025-04-30 09:53:29 -0400

    update

--- pizza.py
+++ pizza.py
@@ -1,10 +1,2 @@
 def start_order():
     print("Welcome to Collaborative Pizza!")
-
-
-def add_pepperoni():
-    print('Adding pepperoni to the pizza!')
-
-
-def add_pineapple():
-    print('adding pin')
